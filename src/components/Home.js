7de1534e1896136c1da3b7d73c37d0d6ff058716
Jason Parker <Jbparker243@gmail.com>
2022-04-05 10:11:46 -0500

    made Nav feed into Home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Home() {
+  return (
+    <div>
+      <div>ALABAMA MELEE</div>
+      <Nav />
+    </div>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Regions</li>
+          <li>Social</li>
+          <li>Calendar</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
