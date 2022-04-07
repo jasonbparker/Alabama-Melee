6e61c44bebe427d9b4dff23521109bf5ff62be14
Jason Parker <Jbparker243@gmail.com>
2022-04-07 11:00:26 -0500

    test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,11 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>Regions</li>
-          <li>Social</li>
-          <li>Calendar</li>
-        </ul>
-      </nav>
-    </header>
+    <select name="selectList" id="selectList">
+        <option value="regions">regions</option>
+      <option value="Birmingham">Birmingham</option>
+      <option value="Tuscaloosa">Tuscaloosa</option>
+      <option value="Huntsville">Huntsville</option>
+    </select>
   );
 }
---
+++ src/regions/Birmingham.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Birmingham() {
+  return (
+    <div>
+      <div>BHAM MELEEs</div>
+    </div>
+  );
+}
